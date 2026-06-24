2f9b5aa3d32b5a1777532e7c20d40cfe4bd1c6f6
Lucas Manoel <lucasmanoel.email@gmail.com>
2026-06-24 13:23:15 -0300

    Time: 0 ms (100%), Space: 43.4 MB (17.14%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        String prefixo = strs[0];
+
+        for(int i = 0; i <strs.length; i++){
+            while(strs[i].indexOf(prefixo) !=0){
+                prefixo = prefixo.substring(0, prefixo.length() -1);
+
+                if(prefixo.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefixo;
+    }
+}
